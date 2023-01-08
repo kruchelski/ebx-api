a55338c1ef304dd8c6121e6724db73e0eeb416c7
Cassiano Kruchelski <casst@protonmail.com>
2023-01-08 11:23:43 -0300

    add Account class

--- src/helpers/accounts.js
+++ src/helpers/accounts.js
@@ -1,18 +1,33 @@
+class Account {
+  constructor({ id, balance }) {
+    this.id = id;
+    this.balance = balance;
+  }
+
+  addBalance(balance) {
+    this.balance += balance;
+  }
+
+  subtractBalance(balance) {
+    this.balance -= balance;
+  }
+}
+
 class AccountHandler {
   constructor() {
     this.accounts = [];
   }
 
-  addAccount(account) {
-    this.accounts.push(account);
-  }
-
-  listAccounts() {
-    return this.accounts;
+  addAccount(accountDetails) {
+    this.accounts.push(new Account(accountDetails));
   }
 
   resetAccounts() {
     this.accounts = [];
+  }
+
+  listAccounts() {
+    return this.accounts;
   }
 }
 
